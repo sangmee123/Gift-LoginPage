b8321d5f558f557f575e3ce55bb154c1dd593c7e
oeanb12 <oeanb12@gmail.com>
2021-10-09 23:48:44 +0900

    Login page

---
+++ js/loginPage.js
@@ -0,0 +1,25 @@
+//ID와 PASSWORD를 둘 다 입력했을 때 나타나는 반응
+$('#name-input').on('input', checkInput);
+$('#password-input').on('input', checkInput);
+
+function checkInput() {
+    let id = $('#name-input').val();
+    let password = $('#password-input').val();
+    let btnLogin = $('.btn-login');
+
+    if(id === '' || password === '')
+        btnLogin.removeClass('on');
+    else 
+        btnLogin.addClass('on');
+}
+
+//로그인을 한 후 다음 페이지로 넘어감
+$('.btn-login').on('click', function() {
+    let id = $('#name-input').val();
+    let password = $('#password-input').val();
+    
+    if(id === '이건주' && password === '010418') 
+        window.open('https://sangmee123.github.io/Gift/');
+    else if(id !== '이건주' || password !== '010418')
+        alert('아이디 또는 패스워드가 잘못 입력 되었습니다.');
+})
