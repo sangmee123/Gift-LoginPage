2a81f69acfa3ad10e303b93b2ea7bf15357bcca7
oeanb12 <oeanb12@gmail.com>
2021-12-30 10:47:44 +0900

    Correct

--- js/loginPage.js
+++ js/loginPage.js
@@ -19,7 +19,7 @@ $('.btn-login').on('click', function() {
     let password = $('#password-input').val();
     
     if(id === '이건주' && password === '010418') 
-        window.open('https://sangmee123.github.io/Gift/');
+        window.open('https://home-commemoration.netlify.app/');
     else if(id !== '이건주' || password !== '010418')
         alert('아이디 또는 패스워드가 잘못 입력 되었습니다.');
 })
